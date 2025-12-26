2b51ca82be2b24f453eb7697214294d7071431cf
veli_aliyev <veli.aliev@tigahealth.com>
2025-12-26 23:57:50 +0300

    Added logging to CoFlowGwApplication using Lombok's @Slf4j annotation

--- co-flow-gw/src/main/java/com/coflow/gw/CoFlowGwApplication.java
+++ co-flow-gw/src/main/java/com/coflow/gw/CoFlowGwApplication.java
@@ -1,13 +1,16 @@
 package com.coflow.gw;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class CoFlowGwApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoFlowGwApplication.class, args);
+        log.info("CoFlow GW started");
     }
 
 }
